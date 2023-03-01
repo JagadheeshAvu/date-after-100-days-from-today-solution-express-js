cb249e085afd5b91a5d448299833a14c712c19c9
JagadheeshAvu <jagadheesh6349@gmail.com>
2023-03-01 10:00:46 +0000

    Date After Hundered Days From Today

---
+++ app.js
@@ -0,0 +1,17 @@
+const addDays = require("date-fns/addDays");
+
+const express = require("express");
+const app = express();
+
+app.get("/", (request, response) => {
+  const newDate = new Date();
+  const result = addDays(
+    new Date(newDate.getFullYear(), newDate.getMonth(), newDate.getDate()),
+    100
+  );
+  response.send(
+    `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`
+  );
+});
+
+module.exports = app;
